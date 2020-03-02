376d67b25c7b79334941ed1a3c56807c98042171
Kyle <rankakuhub@users.github.com>
2020-03-02 09:15:01 +0000

    first commit

---
+++ Scripts/Hub2.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Hub2 : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(4);
+        }
+
+    }
+
+}
